0d0f569c34633881222e37f2f4843add9962df4e
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-15 16:00:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = 'hanson-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/1653/1653630.png'
+];
+
+// App install hote waqt files save karna
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Offline mode mein files yahan se uthana
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
